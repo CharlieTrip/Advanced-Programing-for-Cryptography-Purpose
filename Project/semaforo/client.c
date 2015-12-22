3546df317335e17a475c728a607e40feab86f297
Carlo Brunetta <brunocarletta@gmail.com>
2015-12-22 11:28:57 +0100

    Init
    + Semaphore from Budro (has to be updated)

---
+++ Project/semaforo/client.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+/* Ci sono due file di testo che vengono usati: uno è canale.txt 
+ 	che non è altro il file in cui vengono scritte le varie "cose",
+ 	l'altro è semaforo.txt: questi file viene creato prima e 
+ 	cancellato dopo ogni comunicazione. Dunque il server (per esempio)
+ 	verifica prima se il file è presente: se lo è allora non fa nulla,
+ 	se non lo è allora può scrivere. Stessa cosa fa il client
+*/
+
+int main(){
+
+	int count = 0;
+
+	time_t endwait;
+    time_t start = time(NULL);
+    time_t seconds = 10;
+    endwait = start + seconds;
+
+    FILE *canale; 
+    fopen("semaforo.txt","w");
+    canale = fopen("canale.txt","w");
+    fprintf(canale, "client %d \n",count++);
+	fclose(canale);
+	remove("semaforo.txt");
+	sleep(1);
+
+
+while(start < endwait){
+
+
+	start = time(NULL);
+
+	if (access("semaforo.txt", F_OK) != -1){  // verifico se il file semaforo.txt è presente
+	}
+	else{ // se non è presente faccio i vari comandi
+		fopen("semaforo.txt","w");
+		canale = fopen("canale.txt","a");
+		fprintf(canale, "client %d \n",count++);
+		fclose(canale);
+		remove("semaforo.txt");
+		sleep(1);	
+	}
+}
+
+
+
+return 0;
+}
